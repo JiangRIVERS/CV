01eb36556fa28bcff022d6dd7151f7545ec5c098
JIANG mingda <jiangrivers9511@gmail.com>
2019-12-05 15:24:07 +0800

    Update FPN.py

--- Detection/Mask R-CNN/FPN.py
+++ Detection/Mask R-CNN/FPN.py
@@ -9,4 +9,27 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+class Bottleneck(nn.Module):
+    expansion=4
+
+    def __init__(self,inplanes,planes,stride=1,downsample=False):
+        super(Bottleneck, self).__init__()
+        self.conv1=nn.Conv2d(inplanes,planes,kernel_size=1,stride=1,padding=0,bias=False)
+        self.bn1=nn.BatchNorm2d(planes)
+        self.conv2=nn.Conv2d(planes,planes,kernel_size=3,stride=stride,padding=1,bias=False)
+        self.bn2=nn.BatchNorm2d(planes)
+        self.conv3=nn.Conv2d(planes,self.expansion*planes,kernel_size=1,stride=1,padding=0,bias=False)
+        self.bn3=nn.BatchNorm2d(self.expansion*planes)
+        self.relu = nn.ReLU(inplace=True)
+
+        if downsample:
+            self.dowsample=nn.Sequential(
+                nn.Conv2d(inplanes,self.expansion*planes,kernel_size=1,stride=stride,padding=0,bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self,x):
+
+
+
 
